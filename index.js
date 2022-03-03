35a85cb7d09d9f107dcdf76806e79ab74f47e378
Gogs <649536862@qq.com>
2022-03-02 17:16:47 -0800

    'bot'

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
 const db = require ('mongoose')
+const bodyParser = require("body-parser");
 const bp = require ('./models/m')
 const express = require('express')
-
 const app = express()
-const port = 80
-db.connect("mongodb+srv://lucas:zlc20100303@cluster0.xf2ly.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", { useNewUrlParser: true })
+const port = 60000
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.set('view engine', 'ejs')
+db.connect("mongodb+srv://lucas:zlc20100303@cluster0.xf2ly.mongodb.net/blogdb?retryWrites=true&w=majority", { useNewUrlParser: true })
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-    bp.create({t:'hello',b:'World'},(err,blpo)=>{console.log(err,blpo)})
-})
-
+    res.redirect('/posts')
+});
+app.get('/posts', (req, res) => {
+    bp.find({},(err,data)=>{
+        if (err) {throw err}
+        res.render('posts',{data:data})
+    });
+});
+app.get('/new', (req, res) => {
+    res.render('newpost')
+});
+app.post('/post',(req,res) => {
+    console.log(req.body);
+    bp.create({t:req.body.title,b:req.body.textbody,u:req.body.urls},(err,blpo)=>{console.log(err,blpo)})
+    res.redirect('/')
+});
+app.post('/del',(req,res) => {
+    console.log(req.body);
+    bp.findByIdAndRemove(req.body.id,(err,blpo)=>{console.log(err,blpo)})
+    res.redirect('/')
+});
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
-}) //hello
+});
+Infinity;Infinity;Infinity;Infinity;Infinity;Infinity;Infinity;Infinity;
--- models/m.js
+++ models/m.js
@@ -1,6 +1,6 @@
 const db = require ('mongoose')
 const sch=db.Schema;
 
-const bps = new sch({t:String,b:String})
+const bps = new sch({t:String,b:String,u:String})
 const bp=db.model('BlogPost',bps)
 module.exports=bp

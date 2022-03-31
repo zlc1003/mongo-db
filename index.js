1f5197778505761c7ab30f34211d279d5e404e6a
zlc1003 <91501777+zlc1004@users.noreply.github.com>
2022-03-31 10:59:28 -0700

    'a

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+/* ---------------------------------password---------------------------- */
+var NEWS_PAPER_ADMIN_PASSW = "doyouknow-west-bay-news-paper-admin-password"
+/* --------------------------------------------------------------------- */
+
 const db = require('mongoose')
 const bodyParser = require("body-parser");
 const bp = require('./models/m')
@@ -29,7 +33,7 @@ app.use(session({
     saveUninitialized: true,
     cookie: { secure: false }
 }))
-db.connect("mongodb+srv://lucas:zlc20100303@cluster0.xf2ly.mongodb.net/blogdb?retryWrites=true&w=majority", { useNewUrlParser: true })
+db.connect("mongodb+srv://lucas:zlc20100303@cluster0.xf2ly.mongodb.net/news_paper?retryWrites=true&w=majority", { useNewUrlParser: true })
 //aasasd
 app.get('/post/:id', (req, res) => {
     console.log(req.params.id)
@@ -56,75 +60,24 @@ app.get('/posts', (req, res) => {
     });
 });
 
-app.get('/new', (req, res) => {
-    res.render('newpost')
+app.get('/admin/new', (req, res) => {
+    res.render('newpost');//, { pwd: "doyouknow-west-bay-news-paper-admin-password" })
 });
 
-app.post('/login', (req, res) => {
-    hash=req.body.pwd
-    bp.user.findOne({ name: req.body.user}, (err, data) => {
-        if (err) throw err
-        if (data == null || data == undefined) {
-            res.render('login', { data: null })
-        }
-        else {
-            bcrypt.compare(req.body.pwd, data.pwd, function (err, result) {
-                if (err) throw err
-                if (result) {
-                    req.session.userid = data._id
-                    res.redirect('/posts')
-                }
-                else {
-                    res.render('login', { data: null })
-                }
-            });
-        }
-    });
-})
-app.get('/login', (req, res) => {
-    res.render('login', { data: null })
-})
-app.post('/register', (req, res) => {
-    bp.user.findOne({ name: req.body.user }, (err, data) => {
-        if (err) { throw err }
-        if (data == null || data == undefined) {
-            bcrypt.hash(req.body.pwd, 10, function (err, hash) {
-                if (err) {
-                    throw err;
-                }
-                bp.user.create({ name: req.body.user, pwd: hash }, (err, data) => {
-                    if (err) { throw err }
-                    res.redirect('/login')
-                })
-            });
-        }
-        else {
-            res.render('login', { data: null })
-        }
-    });
-})
-app.get('/register', (req, res) => {
-    res.render('register', { data: null })
-})
-app.get('/logout', (req, res) => {
-    res.clearCookie('userid')
-    req.session.destroy(
-        function (err) {
-            if (err) {
-                throw err
-            }
-            res.redirect('/login')
-        }
-    )
-})
 app.post('/post', (req, res) => {
     console.log(req.body);
     url = req.body.urls;
-    if (url.indexOf('https://') == -1) {
+    if (url.indexOf('https://') == -1 && url!='') {
         url = 'https://' + url;
     }
+    else if (url == '') {
+        url = '/#'
+    }
+    console.log(req.body.urls);
     ppp = req.body.moreinfo
     ppp = ppp.replace(/\r\n/g, '\n')
+    pwd = req.body.pwd
+    if (pwd != NEWS_PAPER_ADMIN_PASSW) { res.redirect('/404'); return; }
     bp.post.create({ t: req.body.title, b: req.body.textbody, u: url, m: ppp }, (err, blpo) => { console.log(err, blpo) })
     res.redirect('/')
 });
